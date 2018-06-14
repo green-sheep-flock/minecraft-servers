5efaf22808fe8b89e42caf22e86b8e89bb9b50a4
Rob Rua <robertrua@gmail.com>
2018-06-14 02:20:58 -0400

    Update start.sh

--- vanilla/start.sh
+++ vanilla/start.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-java -jar -Xms$MINECRAFT_XMS -Xmx$MINECRAFT_XMX -d64 -jar /opt/minecraft/data/server.jar -o true nogui
+cd /opt/minecraft/data && java -jar -Xms$MINECRAFT_XMS -Xmx$MINECRAFT_XMX -d64 -jar /opt/minecraft/data/server.jar -o true nogui
